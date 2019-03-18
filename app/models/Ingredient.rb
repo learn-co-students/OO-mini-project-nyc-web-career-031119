5e1d4a1ff9523fa1269e68e02338b4ddd2e987b7
github username <github email address>
2019-03-18 17:00:07 -0400

    done with Paul R

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,26 @@
+class Ingredient
 
+  attr_accessor :name
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def users
+    Allergen.all.select{ |allergen| allergen.ingredient == self }
+  end
+
+  def self.allergens
+    all.map{|ingredient| ingredient.users}.flatten
+  end
+
+  def self.most_common_allergen
+    all.sort_by{|ingredient| ingredient.users.length}[-1]
+  end
+end
