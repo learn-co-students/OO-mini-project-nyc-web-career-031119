c74e9fd65c8c8f02ef2f0d8bcf957827a0b39ef6
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2019-03-18 18:16:33 -0400

    done w/ theo

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,25 @@
+class Ingredient
 
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name	
+		@@all << self
+	end
+
+	@@all = []
+
+	def self.all
+		@@all
+	end
+
+	def self.most_common_allergen
+		#return an Ingrendient instance
+		counts = Hash.new 0
+
+		Allergen.all.each do |instance|
+		  counts[instance.ingredient] += 1
+		end
+		counts.max_by {|ingredient, count| count}[0]
+	end
+end
